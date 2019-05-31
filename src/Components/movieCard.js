d6162a6a88f56df2f1f8dd8023fb0f6c2280cbfa
kanbd <kanbdsh@gmail.com>
2019-05-30 20:58:13 -0700

    modify the movecard to add button function

--- src/Components/movieCard.js
+++ src/Components/movieCard.js
@@ -1,7 +1,25 @@
 import React from 'react';
-
+import { connect } from 'react-redux'
+import {ADD} from '../redux/actions';
 class MoiveCard extends React.Component{
+  handleClickAdd=()=>{
+    this.props.dispatch(ADD({data:this.props.MoiveData,index:this.props.index}))
+  }
+  handleClickRemove=()=>{
+    this.props.dispatch(ADD({data:this.props.MoiveData,index:this.props.index}))
+  }
   render() {
-    return <div>best moive</div>;
+    return <div>
+      <div>
+        <img src={this.props.MoiveData.img} alt="moviepost"/>
+        <p>{this.props.MoiveData.title}</p>
+      </div>
+      {
+        this.props.type === 'mylist' ?
+        <button onClick={this.handleClickRemove}>remove</button>:
+        <button onClick={this.handleClickAdd}>add</button>
+      }
+    </div>;
   }
 }
+export default connect()(MoiveCard);
