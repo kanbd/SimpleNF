e6bcbfef0247e526ac132d0819306c5f03bc658c
kanbd <kanbdsh@gmail.com>
2019-05-30 20:58:50 -0700

    modify the action and reducer to uisng the redux pass data

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -6,9 +6,12 @@ const initialState = {
 export default function simpleApp(state = initialState, action) {
   switch (action.type) {
     case 'REOMVE':
-      return Object.assign({}, state, {
-        mylist: action.data
-      })
+        let arr = state.mylist;
+        arr.splice(action.index,1);
+        return Object.assign({}, state, {
+          mylist: arr,
+          recommendations: [...state.recommendations, action.data]
+        });
     case 'ADD':
       let arr = state.recommendations;
       arr.splice(action.index,1);
