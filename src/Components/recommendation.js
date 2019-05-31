9da4e3cdf162ff2fab7030b12c725f1b7dce1b22
kanbd <kanbdsh@gmail.com>
2019-05-30 22:10:48 -0700

    import style file inot mylist and recommendation component

--- src/Components/recommendation.js
+++ src/Components/recommendation.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import MovieCard from './movieCard'
 import { connect } from 'react-redux';
+import '../css/listcontainer.css'
 class Recommendation extends React.Component{
   render() {
-    return <div>best Recommendation
+    return <div className="list-wrapper">
+      <h3>Recommendation</h3>
+      <div className="flex-container">
       {this.props.store.recommendations.map((data,index)=>{
         return <MovieCard type="recommendation" key={index} index={index} MoiveData={data} />;
       })}
+      </div>
     </div>;
   }
 }
