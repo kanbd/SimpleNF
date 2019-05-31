9b8b315f6d6f0da0c232a1d67ce062427cb8b4aa
kanbd <kanbdsh@gmail.com>
2019-05-30 22:09:31 -0700

    import the style into moviecard component

--- src/Components/movieCard.js
+++ src/Components/movieCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import {ADD,REMOVE} from '../redux/actions';
+import '../css/card.css';
 class MoiveCard extends React.Component{
   handleClickAdd=()=>{
     this.props.dispatch(ADD({data:this.props.MoiveData,index:this.props.index}))
@@ -10,15 +11,19 @@ class MoiveCard extends React.Component{
   }
   render() {
     return <div>
-      <div>
+      <div className="img-wrapper">
         <img src={this.props.MoiveData.img} alt="moviepost"/>
+        <div className="button-wrapper">
+          {
+            this.props.type === 'mylist' ?
+            <button onClick={this.handleClickRemove}>Remove</button>:
+            <button onClick={this.handleClickAdd}>Add</button>
+          }
+        </div>
+      </div>
+      <div className="title-wrapper">
         <p>{this.props.MoiveData.title}</p>
       </div>
-      {
-        this.props.type === 'mylist' ?
-        <button onClick={this.handleClickRemove}>remove</button>:
-        <button onClick={this.handleClickAdd}>add</button>
-      }
     </div>;
   }
 }
