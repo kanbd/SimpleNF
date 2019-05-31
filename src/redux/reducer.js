ce58bf56d8d6901b87b3b856859dc5d128a38284
kanbd <kanbdsh@gmail.com>
2019-05-30 20:37:08 -0700

    modify reducer to create add and store_data case

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,12 +3,24 @@ const initialState = {
   recommendations:[]
 }
 
-function simpleApp(state = initialState, action) {
+export default function simpleApp(state = initialState, action) {
   switch (action.type) {
-    case REOMVE:
-      return null;
-    case ADD:
-      return null;
+    case 'REOMVE':
+      return Object.assign({}, state, {
+        mylist: action.data
+      })
+    case 'ADD':
+      let arr = state.recommendations;
+      arr.splice(action.index,1);
+      return Object.assign({}, state, {
+        mylist: [...state.mylist, action.data],
+        recommendations: arr
+      });
+    case 'STORE_DATA':
+        return Object.assign({}, state, {
+          mylist: action.mylist,
+          recommendations: action.recommendations
+        });
     default:
       return state
     }
